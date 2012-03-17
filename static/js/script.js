b6a3b76116132cf6ec37172504a26cdd7d0916a4
Nicolas Clerc <kernicPanel@nclerc.fr>
2012-03-17 16:33:24 +0100

    Revert "Merge branch 'backbone'"
    This reverts commit db119ddeb93d614a0a6446b9df5cc71f22f268f0, reversing
    changes made to e8f96d36afb361fc13d7e1b53bb10df8f1be80d5.

--- static/js/script.js
+++ static/js/script.js
@@ -3,17 +3,13 @@
 
 $(document).ready(function() {   
 
-    var socket = io.connect();
+  var socket = io.connect();
 
-    $('#sender').bind('click', function() {
-        socket.emit('message', 'Message Sent on ' + new Date());     
-    });
+  $('#sender').bind('click', function() {
+   socket.emit('message', 'Message Sent on ' + new Date());     
+  });
 
-    socket.on('server_message', function(data){
-        $('#reciever').append('<li>' + data + '</li>');  
-    });
-
-    socket.on('log', function(data){
-        console.log("data : ", data);
-    });
+  socket.on('server_message', function(data){
+   $('#reciever').append('<li>' + data + '</li>');  
+  });
 });
