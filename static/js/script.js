327d5585b0ea1e6de3f74d84f6ef6e185195eff4
Nicolas Clerc <kernicPanel@nclerc.fr>
2012-03-21 01:15:24 +0100

    first stucture - redmine fetch async

--- static/js/script.js
+++ static/js/script.js
@@ -3,13 +3,21 @@
 
 $(document).ready(function() {   
 
-  var socket = io.connect();
+    var socket = io.connect();
 
-  $('#sender').bind('click', function() {
-   socket.emit('message', 'Message Sent on ' + new Date());     
-  });
+    $('#sender').bind('click', function() {
+        socket.emit('message', 'Message Sent on ' + new Date());     
+    });
 
-  socket.on('server_message', function(data){
-   $('#reciever').append('<li>' + data + '</li>');  
-  });
+    socket.on('server_message', function(data){
+        $('#reciever').append('<li>' + data + '</li>');  
+    });
+
+    socket.on('log', function(data){
+        console.log("data : ", data);
+    });
+
+    socket.on('redmine::connect', function(data){
+        socket.emit("redmine::sync");
+    });
 });
