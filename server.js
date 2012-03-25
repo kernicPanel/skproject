227aea54ee40ccc4b9ea92fd063e6cdd71b3a706
Nicolas Clerc <kernicPanel@nclerc.fr>
2012-03-25 01:47:46 +0100

    backbone setup

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ server.configure(function(){
     server.use(connect.bodyParser());
     server.use(express.cookieParser());
     server.use(express.session({ secret: "shhhhhhhhh!"}));
-    server.use(connect.static(__dirname + '/static'));
+    server.use(connect.static(__dirname + '/assets'));
     server.use(server.router);
 });
 
