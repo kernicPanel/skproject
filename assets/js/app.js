1a7ce1181c46c0ef057e969168651770151ebc8e
Nicolas Clerc <kernicPanel@nclerc.fr>
2012-03-25 22:54:12 +0200

    users's issues

--- assets/js/app.js
+++ assets/js/app.js
@@ -39,12 +39,60 @@ $(function() {
             var html = ich.skuser({
                 name: skuser.get('name')
             });
-            $(html).attr('id', skuser.get('_id'));
+            $(html).attr('id', 'skuser-' + skuser.get('_id'));
             $(this.el).append(html);
         }
     });
 
     window.skuserView = new SkUserView();
+
+    window.Issue = Backbone.Model.extend({
+        initialize: function() {
+        }
+    });
+
+    var IssueList = Backbone.Collection.extend({
+        model: Issue
+    });
+
+
+    window.IssueView = Backbone.View.extend({
+
+        el: $('.content'),
+
+        // The TodoView listens for changes to its model, re-rendering.
+        initialize: function() {
+            _.bindAll(this, 'render', 'setUser', 'addIssue', 'appendIssue'); // remember: every function that uses 'this' as the current object should be in here
+
+            this.collection = new IssueList();
+            this.collection.bind('add', this.appendIssue); // collection event binder
+
+            this.render();
+        },
+
+        // Re-render the contents of the todo item.
+        render: function() {
+            console.log("render : ");
+            console.log("this.el : ", this.el);
+            return this;
+        },
+        setUser: function(userId) {
+            //this.collection.add(issue); // add issue to collection; view is updated via event 'add'
+            this.el = $('#skuser-' + userId + ' ul.issues');
+        },
+        addIssue: function(issue) {
+            this.collection.add(issue); // add issue to collection; view is updated via event 'add'
+        },
+        appendIssue: function(issue){
+            var html = ich.issue({
+                subject: issue.get('subject')
+            });
+            $(html).attr('id', 'issue-' + issue.get('_id'));
+            $(this.el).append(html);
+        }
+    });
+
+    //window.issueView = new IssueView();
 
     socket.on('redmine::connect', function(data){
         console.log("redmine connect : ");
@@ -59,6 +107,18 @@ $(function() {
                 skuser = new SkUser();
                 skuser.set(data[i]);
                 skuserView.addUser(skuser);
+
+                var loopCount2 = data[i].redmine.issues.length;
+                for (var j = 0; j < loopCount2; j++) {
+                    var curIssue = data[i].redmine.issues[j];
+                    console.log("curIssue : ", curIssue);
+                    var issueView = new IssueView();
+                    issueView.setUser(skuser.get('_id'));
+                    var issue = new Issue();
+                    issue.set(curIssue);
+                    issueView.addIssue(issue);
+                }
+                delete loopCount2;
             }
             delete loopCount;
         });
