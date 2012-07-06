587b99e60f0524a502926bd323643e8099ee1165
kernicPanel <kernicpanel@nclerc.fr>
2012-07-06 21:29:05 +0200

    display extracted data

--- assets/js/appExtract.js
+++ assets/js/appExtract.js
@@ -22,9 +22,12 @@ var app = {
     socket.on('redmine::connect', function(data){
         console.log("redmine connect : ");
 
-        socket.emit('redmineExtract::sync', function (data) {
-          console.log(data);
+        $('#syncIssues').click(function() {
+            socket.emit('redmineExtract::sync', function (data) {
+              console.log(data);
+            });
         });
+
         /*
          *socket.emit('redmineExtract::getProjects', function(err, datas) {
          *    console.log("err : ", err);
@@ -37,49 +40,51 @@ var app = {
          *    delete loopCount;
          *});
          */
+        $('#getIssues').click(function() {
+            var $extract = $('#extract tbody');
+            $extract.html('');
+            socket.emit('redmineExtract::getIssues', function(err, datas) {
+                var issuesHtml = [];
 
-/*
- *        socket.emit('redmineExtract::getIssues', function(err, datas) {
- *            var issuesHtml = [];
- *            var $extract = $('#extract tr');
- *            console.log("err : ", err);
- *            console.log("data : ", data);
- *            var loopCount = datas.length;
- *            for (var i = 0; i < loopCount; i++) {
- *                var data = datas[i];
- *
- *                console.log("data : ", data);
- *
- *                var issueHtml = ich.issue({
- *                    nomSousProjet: data.project.name,
- *                    numTache: data,
- *                    nomTache: data,
- *                    dateDemande: data, 
- *                    userDateDemande: data,
- *                    dateFirstPost: data,
- *                    userDateFirstPost: data,
- *                    time: data,
- *                    dateAValider: data,
- *                    dateLivre: data,
- *                    dateALivrer: data,
- *                    dateFerme: data
- *                });
- *
- *                issuesHtml = issuesHtml.concat(issueHtml);
- *                //console.log("issueHtml : ", issueHtml);
- *                $extract.after(issueHtml);
- *
- *            }
- *            delete loopCount;
- *            console.log("$extract : ", $extract);
- *            glob = issuesHtml;
- *            test = $extract;
- *            //$extract.after(issuesHtml);
- *        });
- */
+                console.log("err : ", err);
+                console.log("datas[0] : ", datas[0]);
+                var loopCount = datas.length;
+                for (var i = 0; i < loopCount; i++) {
+                    var data = datas[i];
+
+                    //console.log("data : ", data);
+
+                    var issueHtml = ich.issue({
+                        nomSousProjet: data.project,
+                        numTache: data.id,
+                        nomTache: data.name,
+                        dateDemande: data, 
+                        userDateDemande: data,
+                        dateFirstPost: data,
+                        userDateFirstPost: data,
+                        time: data,
+                        dateAValider: data,
+                        dateLivre: data,
+                        dateALivrer: data,
+                        dateFerme: data
+                    });
+
+                    issuesHtml = issuesHtml.concat(issueHtml);
+                    //console.log("issueHtml : ", issueHtml);
+                    $extract.append(issueHtml);
+
+                }
+                delete loopCount;
+                console.log("$extract : ", $extract);
+                glob = issuesHtml;
+                test = $extract;
+                //$extract.after(issuesHtml);
+            });
+        });
+
 
         socket.on('log', function(data){
-            console.log("data : ", data);
+            console.log("node log : ", data);
         });
     });
   }
