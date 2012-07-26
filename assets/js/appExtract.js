65f6d2504f27163926bf658cbda7c9035bc05e2d
kernicPanel <kernicpanel@nclerc.fr>
2012-07-26 17:17:14 +0200

    stats report

--- assets/js/appExtract.js
+++ assets/js/appExtract.js
@@ -52,12 +52,34 @@ var app = {
         $('#getIssues').click(function() {
             var $extract = $('#extract tbody');
             $extract.html('');
+            $('#statsReport').html('');
+            display.resetStats();
             $('#dialog-message').html('getting datas');
             socket.emit('redmineExtract::getIssues', function(err, datas) {
                 //$('#dialog-message').html('');
                 ////console.log("err : ", err);
                 //console.log("datas[0] : ", datas[0]);
                 //display.issuesArray(datas);
+            });
+        });
+
+        $('#getGarantie').click(function() {
+            var $extract = $('#extract tbody');
+            $extract.html('');
+            $('#statsReport').html('');
+            display.resetStats();
+            $('#dialog-message').html('getting datas');
+            socket.emit('redmineExtract::getGarantie', function(err, datas) {
+            });
+        });
+
+        $('#getSupport').click(function() {
+            var $extract = $('#extract tbody');
+            $extract.html('');
+            $('#statsReport').html('');
+            display.resetStats();
+            $('#dialog-message').html('getting datas');
+            socket.emit('redmineExtract::getSupport', function(err, datas) {
             });
         });
 
@@ -115,7 +137,7 @@ var display = (function () {
  *    statuses.ferme = 5;
  */
 
-    var sumTimeEntry = function(total) {
+    var round2decimals = function(total) {
         return Math.round(total * 100) / 100;
     };
 
@@ -131,9 +153,73 @@ var display = (function () {
         }
     };
 
+    var nbDemandes1h = 0;
+    var nbDemandes2h = 0;
+    var nbDemandes3h = 0;
+    var nbDemandesMore = 0;
+    var nbDemandesTotal = 0;
+
+    var ratio1h = 0;
+    var ratio2h = 0;
+    var ratio3h = 0;
+    var ratioMore = 0;
+    var ratioTotal = 0;
+
+    var moyenneHeure1h = 0;
+    var moyenneHeure2h = 0;
+    var moyenneHeure3h = 0;
+    var moyenneHeureMore = 0;
+    var moyenneHeureTotal = 0;
+
+    var moyenneJour1h = 0;
+    var moyenneJour2h = 0;
+    var moyenneJour3h = 0;
+    var moyenneJourMore = 0;
+    var moyenneJourTotal = 0;
+
+    var somme1h = 0;
+    var somme2h = 0;
+    var somme3h = 0;
+    var sommeMore = 0;
+    var sommeTotal = 0;
+
+    publicAccess.resetStats = function() {
+        nbDemandes1h = 0;
+        nbDemandes2h = 0;
+        nbDemandes3h = 0;
+        nbDemandesMore = 0;
+        nbDemandesTotal = 0;
+
+        ratio1h = 0;
+        ratio2h = 0;
+        ratio3h = 0;
+        ratioMore = 0;
+        ratioTotal = 0;
+
+        moyenneHeure1h = 0;
+        moyenneHeure2h = 0;
+        moyenneHeure3h = 0;
+        moyenneHeureMore = 0;
+        moyenneHeureTotal = 0;
+
+        moyenneJour1h = 0;
+        moyenneJour2h = 0;
+        moyenneJour3h = 0;
+        moyenneJourMore = 0;
+        moyenneJourTotal = 0;
+
+        somme1h = 0;
+        somme2h = 0;
+        somme3h = 0;
+        sommeMore = 0;
+        sommeTotal = 0;
+    };
+
+    var $extract = $('#extract tbody');
+    var $stats = $('#statsReport');
 
     publicAccess.issuesArray = function (datas) {
-        var $extract = $('#extract tbody');
+        //var $extract = $('#extract tbody');
         var loopCount = datas.length;
         moment.lang('fr');
         for (var i = 0; i < loopCount; i++) {
@@ -144,7 +230,6 @@ var display = (function () {
     };
 
     publicAccess.issue = function (issue) {
-        var $extract = $('#extract tbody');
         //console.log("issue : ", issue);
         moment.lang('fr');
         var dateDemande = formatMoment(issue.created_on);
@@ -198,20 +283,103 @@ var display = (function () {
             delaiFermeJourOuvre: delaiFermeJourOuvre,
             delaiRejeteJourOuvre: delaiRejeteJourOuvre,
 
-            time: sumTimeEntry(issue.time_entriesTotal)
+            time: round2decimals(issue.time_entriesTotal)
         });
+
+        var excluded = false;
         if (delaiFirstPostJourOuvre === 0) {
             $(issueHtml).addClass('excluded');
+            excluded = true;
         }
         if (dateFirstPost === dateFerme || dateFirstPost === dateRejete) {
             //console.log("directClose : ", issue.id, issue);
             $(issueHtml).addClass('excluded directClose');
+            excluded = true;
         }
-        if (issue.stats.userFirstPost === issue.author.name) {
-            //console.log("directClose : ", issue.id, issue);
-            $(issueHtml).addClass('sameUser');
-        }
+        /*
+         *if (issue.stats.userFirstPost === issue.author.name) {
+         *    $(issueHtml).addClass('sameUser');
+         *}
+         */
         $extract.append($(issueHtml));
+
+        /*
+         *$('.delaiFirstPostJourOuvre').each(function(index) {
+         *        alert(index + ': ' + $(this).text());
+         *});
+         */
+
+        if (!excluded) {
+                
+            if (delaiFirstPostJourOuvre < 1) {
+                nbDemandes1h++;
+                somme1h += delaiFirstPostJourOuvre;
+            }
+            else if (delaiFirstPostJourOuvre < 2) {
+                nbDemandes2h++;
+                somme2h += delaiFirstPostJourOuvre;
+            }
+            else if (delaiFirstPostJourOuvre < 3) {
+                nbDemandes3h++;
+                somme3h += delaiFirstPostJourOuvre;
+            }
+            //else if (delaiFirstPostJourOuvre > 3) {
+            else {
+                nbDemandesMore++;
+                sommeMore += delaiFirstPostJourOuvre;
+            }
+            sommeTotal += delaiFirstPostJourOuvre;
+
+            ratio1h = round2decimals(nbDemandes1h / nbDemandesTotal);
+            ratio2h = round2decimals(nbDemandes2h / nbDemandesTotal);
+            ratio3h = round2decimals(nbDemandes3h / nbDemandesTotal);
+            ratioMore = round2decimals(nbDemandesMore / nbDemandesTotal);
+            //ratioTotal = round2decimals(nbDemandesTotal / nbDemandesTotal);
+            ratioTotal = round2decimals( (nbDemandes1h + nbDemandes2h + nbDemandes3h + nbDemandesMore ) / nbDemandesTotal );
+
+            moyenneHeure1h = round2decimals(somme1h / nbDemandes1h);
+            moyenneHeure2h = round2decimals(somme2h / nbDemandes2h);
+            moyenneHeure3h = round2decimals(somme3h / nbDemandes3h);
+            moyenneHeureMore = round2decimals(sommeMore / nbDemandesMore);
+            moyenneHeureTotal = round2decimals(sommeTotal / nbDemandesTotal);
+
+            moyenneJour1h = round2decimals(somme1h / nbDemandes1h / 7);
+            moyenneJour2h = round2decimals(somme2h / nbDemandes2h / 7);
+            moyenneJour3h = round2decimals(somme3h / nbDemandes3h / 7);
+            moyenneJourMore = round2decimals(sommeMore / nbDemandesMore / 7);
+            moyenneJourTotal = round2decimals(sommeTotal / nbDemandesTotal / 7);
+
+
+            var stats = ich.stats({
+                nbDemandes1h: nbDemandes1h,
+                ratio1h: ratio1h,
+                moyenneHeure1h: moyenneHeure1h,
+                moyenneJour1h: moyenneJour1h,
+
+                nbDemandes2h: nbDemandes2h,
+                ratio2h: ratio2h,
+                moyenneHeure2h: moyenneHeure2h,
+                moyenneJour2h: moyenneJour2h,
+
+                nbDemandes3h: nbDemandes3h,
+                ratio3h: ratio3h,
+                moyenneHeure3h: moyenneHeure3h,
+                moyenneJour3h: moyenneJour3h,
+
+                nbDemandesMore: nbDemandesMore,
+                ratioMore: ratioMore,
+                moyenneHeureMore: moyenneHeureMore,
+                moyenneJourMore: moyenneJourMore,
+
+                nbDemandesTotal: ++nbDemandesTotal,
+                ratioTotal: ratioTotal,
+                moyenneHeureTotal: moyenneHeureTotal,
+                moyenneJourTotal: moyenneJourTotal
+            });
+            $stats.html(stats);
+
+        }
+
     };
 
     return publicAccess;
