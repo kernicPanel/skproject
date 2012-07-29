53f733b238be085d51709e396394660d45c55ae4
Nicolas Clerc <kernicPanel@nclerc.fr>
2012-07-29 10:54:19 +0200

    remane skUser -> teamMember

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,7 +17,7 @@ var app = {
     //Backbone.history.start();
     this.socket = socket = io.connect();
 
-    this.views.skuserView = new this.Views.SkUserView();
+    this.views.skuserView = new this.Views.TeamMemberView();
 
     this.views.skprojectView = new this.Views.SkProjectView();
 
@@ -70,7 +70,7 @@ var app = {
             console.log("getUsersIssues data : ", data);
             var loopCount = data.length;
             for (var i = 0; i < loopCount; i++) {
-                skuser = new app.Models.SkUser();
+                skuser = new app.Models.TeamMember();
                 //skuserView.remove(skuser);
                 skuser.set(data[i]);
                 //skuser.set({id: data[i].login});
