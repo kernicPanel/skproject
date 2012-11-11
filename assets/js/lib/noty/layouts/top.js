a5e11fa24dc1b8d444b14ff2b7f6552a0255b60e
Nicolas Clerc <kernicPanel@nclerc.fr>
2012-11-11 21:56:05 +0100

    move assets js libs to libs folder

---
+++ assets/js/lib/noty/layouts/top.js
@@ -0,0 +1,34 @@
+;(function($) {
+
+	$.noty.layouts.top = {
+		name: 'top',
+		options: {},
+		container: {
+			object: '<ul id="noty_top_layout_container" />',
+			selector: 'ul#noty_top_layout_container',
+			style: function() {
+				$(this).css({
+					top: 40,
+					left: '10%',
+					position: 'fixed',
+					width: '80%',
+					height: 'auto',
+					margin: 0,
+					padding: 0,
+					listStyleType: 'none',
+					zIndex: 9999999
+				});
+			}
+		},
+		parent: {
+			object: '<li />',
+			selector: 'li',
+			css: {}
+		},
+		css: {
+			display: 'none'
+		},
+		addClass: ''
+	};
+
+})(jQuery);
