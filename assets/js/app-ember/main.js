5eb4a9f921a1b65aa8b9b156c22587d0b443b461
Nicolas Clerc <kernicPanel@nclerc.fr>
2013-01-19 16:33:02 +0100

    use yeoman for ember

---
+++ assets/js/app-ember/main.js
@@ -0,0 +1,3 @@
+
+
+var RealTeam = Ember.Application.create();
---
+++ assets/js/app-ember/store.js
@@ -0,0 +1,5 @@
+// Requires Ember-Data
+// App-Ember.Store = DS.Store.extend({
+//   revision: 4,
+//   adapter: DS.RESTAdapter.create()
+// });
